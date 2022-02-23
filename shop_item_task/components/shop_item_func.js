e4928d0bafea8811ae4f3d4b0798b2eebd4fdf06
BorizBritva <10.95.karm@gmail.com>
2022-02-23 23:48:39 +0600

    config_gitignore_and_change_components_code

--- shop_item_task/components/shop_item_func.js
+++ shop_item_task/components/shop_item_func.js
@@ -2,61 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-// function ShopItemFunc(props) {
-//
-//   const {item}=props;
-//
-//   return (
-//     <div className="main-content">
-//       <h2>{item.brand}</h2>
-//       <h1>{item.title}</h1>
-//       <h3>{item.description}</h3>
-//
-//       <div className="description">{item.descriptionFull}</div>
-//
-//       <div className="highlight-window mobile">
-//         <div className="highlight-overlay"></div>
-//       </div>
-//
-//       <div className="divider"></div>
-//
-//       <div className="purchase-info">
-//         <div className="price">{item.currency}{item.price}</div>
-//           <button>Добавить в корзину</button>
-//         </div>
-//     </div>
-//   )
-// }
+function ShopItemFunc(props) {
 
-//export default ShopItemFunc;
+  const {item}=props;
 
-class ShopItemClass extends React.Component {
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
 
-  render() {
+      <div className="description">{item.descriptionFull}</div>
 
-    const {item}=this.props;
-
-    return (
-      <div className="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-
-        <div className="description">{item.descriptionFull}</div>
-
-        <div className="highlight-window mobile">
-          <div className="highlight-overlay"></div>
-        </div>
-
-        <div className="divider"></div>
-
-        <div className="purchase-info">
-          <div className="price">{item.currency}{item.price}</div>
-            <button>Добавить в корзину</button>
-          </div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
       </div>
-    )
-  }
+
+      <div className="divider"></div>
+
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
+    </div>
+  )
 }
 
-export default ShopItemClass;
+ShopItemFunc.propTypes = {
+  brand: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  price: PropTypes.string
+}
+
+// class ShopItemClass extends React.Component {
+//
+//   static propTypes = {
+//     title: PropTypes.instanceOf()
+//   }
+//
+//   render() {
+//
+//     const {item}=this.props;
+//
+//     return (
+//       <div className="main-content">
+//         <h2>{item.brand}</h2>
+//         <h1>{item.title}</h1>
+//         <h3>{item.description}</h3>
+//
+//         <div className="description">{item.descriptionFull}</div>
+//
+//         <div className="highlight-window mobile">
+//           <div className="highlight-overlay"></div>
+//         </div>
+//
+//         <div className="divider"></div>
+//
+//         <div className="purchase-info">
+//           <div className="price">{item.currency}{item.price}</div>
+//             <button>Добавить в корзину</button>
+//           </div>
+//       </div>
+//     )
+//   }
+// }
+//
+// ShopItemClass.propTypes = {
+//   brand: PropTypes.string,
+//   title: PropTypes.string,
+//   description: PropTypes.string,
+//   price: PropTypes.string
+// }
+
+export default ShopItemFunc;
+//export default ShopItemClass;
