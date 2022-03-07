11a993bec25887635b6d34d2934742d3a3b27303
BorizBritva <10.95.karm@gmail.com>
2022-03-07 16:27:41 +0600

    add_icon_to_store

--- task_two/components/Store.js
+++ task_two/components/Store.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CardsView from './product_card/CardsView';
 import ListView from './product_list/ListView';
+import IconSwitch from './icons_switch/IconSwitch';
 
 function Store(props) {
   const product=props.items;
+  const [icon, setIcons] = useState("view_list")
+
+  const handleSwitch = () => {
+    setIcons(prevIcon => {
+      return prevIcon==="view_list" ? "view_module" : "view_list"
+    })
+  }
 
   return (
-    <CardsView items={product} />
+    <>
+      <IconSwitch icon={icon} onSwitch={handleSwitch}/>
+      {icon==="view_module" ? <ListView items={product} /> : <CardsView items={product}/>}
+    </>
   )
 }
 
--- task_two/components/icons_switch/IconSwitch.js
+++ task_two/components/icons_switch/IconSwitch.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 
 function IconSwitch(props) {
+  const {icon, onSwitch} = props;
 
   return (
-
+      <div className="switch-icon">
+        <span className="btn switch-icon-btn" onClick={onSwitch}>
+          <i className={`material-icons ${icon}`}>{icon}</i>
+        </span>
+      </div>
   )
 }
+
+export default IconSwitch;
